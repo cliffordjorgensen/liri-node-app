073d68098a18cf220251ad15c16be5009d1bd4a4
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-06-18 22:09:16 -0700

    imported and installed required modules

--- assets/js/liriApp.js
+++ assets/js/liriApp.js
@@ -1,3 +1,20 @@
-require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+require("dotenv").config(); //set environment variables with dotenv package
+var keys = require("./keys.js"); //import api keys via keys.js
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify); //access spotify key
+var inquirer = require("inquirer"); //import inquirer package
+var axios = require("axios"); //import axios package
+
+
+
+
+// const spotifyCall = function() {
+//     axios
+//         .get()
+// }
+
+// inquirer
+//     .prompt([{
+//         type: "input",
+//         message: ""
+//     }])
