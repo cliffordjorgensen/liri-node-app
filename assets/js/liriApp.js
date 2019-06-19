14fb599bf51291572f2530c26f7c6e454943aad7
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-06-19 00:45:59 -0700

    concert-this function almost done

--- assets/js/liriApp.js
+++ assets/js/liriApp.js
@@ -5,8 +5,6 @@ var spotify = new Spotify(keys.spotify); //access spotify key
 var inquirer = require("inquirer"); //import inquirer package
 var axios = require("axios"); //import axios package
 
-
-
 const main = function() {
     inquirer
         .prompt([{
@@ -16,11 +14,24 @@ const main = function() {
             name: "menuChoice"
         }]).then(function(userChoice) {
             var pickedOP = userChoice.menuChoice
-
             if (pickedOP === 'concert-this') {
-                //make axios call
-                //display shit for the user
+                inquirer
+                    .prompt([{
+                        type: "input",
+                        message: "Please enter the name of an artist or band.",
+                        name: "band"
+                    }]).then(function(artist) {
+                        var bandName = artist.band
+                        axios.get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp")
+                            .then(function(response) {
+                                console.log(response.data[0])
+                            }).catch(function(error) {
+                                console.log("---------------Data---------------");
+                                console.log(error.response.data);
+                            });
+                    })
             } else if (pickedOP === 'spotify-this-song') {
+
                 //make axios call
                 //display shit for the user
             } else if (pickedOP === 'movie-this') {
